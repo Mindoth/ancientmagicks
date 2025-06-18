2ac92aea465e8eb0057b8a50706bb31cddef924b
Mindoth <Mindoth@gmail.com>
2025-06-18 17:32:42 +0300

    New blocks

--- src/main/java/net/mindoth/ancientmagicks/registries/ModBlocks.java
+++ src/main/java/net/mindoth/ancientmagicks/registries/ModBlocks.java
@@ -6,6 +6,8 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,8 +18,30 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AncientMagicks.MOD_ID);
 
+    public static final RegistryObject<Block> BREEZE_ROCK = registerBlock("breeze_rock",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+    public static final RegistryObject<Block> BREEZE_ROCK_STAIRS = registerBlock("breeze_rock_stairs",
+            () -> new StairBlock(() -> ModBlocks.BREEZE_ROCK.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+    public static final RegistryObject<Block> BREEZE_ROCK_SLAB = registerBlock("breeze_rock_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+
+    public static final RegistryObject<Block> BREEZE_BRICKS = registerBlock("breeze_bricks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+    public static final RegistryObject<Block> BREEZE_BRICKS_STAIRS = registerBlock("breeze_brick_stairs",
+            () -> new StairBlock(() -> ModBlocks.BREEZE_ROCK.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+    public static final RegistryObject<Block> BREEZE_BRICKS_SLAB = registerBlock("breeze_brick_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+
     public static final RegistryObject<Block> SPELL_CRAFTING_TABLE = registerBlock("spell_crafting_table",
             () -> new SpellCraftingTableBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
